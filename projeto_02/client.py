4774e87172dc09018891360f54d3e9dba4be2f10
BeHaga <brunohenriqueluz5@gmail.com>
2025-01-14 19:07:59 -0300

    Boas praticas de comentario explicando os trechos dos arquivos server e client

--- projeto_02/client.py
+++ projeto_02/client.py
@@ -9,10 +9,16 @@
 import socket
 
 def main():
+    #.AF_INET para comunicação baseada em IPs no formato protocolo IPv4
+    #.SOCK_STREAM implementação de socket TCP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #esse IP é o padrão de loopback, utilizado em desenvolvimento local
     client_socket.connect(("127.0.0.1", 12345))
+    #1024 bytes é o tamanho máximo a ler por chamada do que vem no socket
     message = client_socket.recv(1024)
+    #.decode() transforma de bytes para string
     print("Mensagem do servidor:", message.decode())
+    #.close() é utilizado para liberar recursos do sistema e informar ao cliente que a comunicação com o servidor foi encerrada
     client_socket.close()
 
 if __name__ == "__main__":
--- projeto_02/server.py
+++ projeto_02/server.py
@@ -4,15 +4,22 @@
 import socket
 
 def main():
+    #.AF_INET para comunicação baseada em IPs no formato protocolo IPv4
+    #.SOCK_STREAM implementação de socket TCP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #.bind() faz associação do socket com IP e Porta
     server_socket.bind(("0.0.0.0", 12345))
+    #.listen() define o tamanho da fila de conexões pendentes, até começar a recusar novas solicitações
     server_socket.listen(5)  # Aguarda até 5 conexões
     print("Servidor iniciado. Aguardando conexões...")
 
     while True:
+        #addr é uma tupla
         client_socket, addr = server_socket.accept()
         print(f"Conexão estabelecida com {addr}")
+        #.send() envio de mensagem ao cliente em formato de bytes
         client_socket.send(b"Bem-vindo ao servidor!")
+        #.close() é utilizado para liberar recursos do sistema e informar ao cliente que a comunicação com o servidor foi encerrada
         client_socket.close()
 
 if __name__ == "__main__":
