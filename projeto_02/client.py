e83aacda3c0730ec4a2e32e147862f99cb870b4f
BeHaga <brunohenriqueluz5@gmail.com>
2025-01-14 16:57:36 -0300

    Insercao da funcao main para o cliente se conectar ao servidor

--- projeto_02/client.py
+++ projeto_02/client.py
@@ -6,3 +6,14 @@
 # - cada cliente deve poder se comunicar com outro cliente usando o nome de usuario (semelhante ao que ocorre no WhatsApp atraves do numero de telefone)
 # - (OPCIONAL) clientes podem se juntar a grupos multicast (semelhante ao que ocorre no whatsapp)
 
+import socket
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("127.0.0.1", 12345))
+    message = client_socket.recv(1024)
+    print("Mensagem do servidor:", message.decode())
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
